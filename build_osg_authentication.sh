9f57494f133a28d43d605a9419fe090393652568
Brandon White <bjwhite@fnal.gov>
2019-09-05 09:06:54 -0500

    Fix build script directory structure.

--- build_osg_authentication.sh
+++ build_osg_authentication.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -z $NO_CACHE_DOCKER_BUILD ]]; then
-    docker build -t osg_authentication osg_authentication/osg_authentication 
+    docker build -t osg_authentication osg_authentication 
 else
-    docker build --no-cache -t osg_authentication osg_authentication/osg_authentication
+    docker build --no-cache -t osg_authentication osg_authentication
 fi
--- build_osg_authentication_base.sh
+++ build_osg_authentication_base.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -z $NO_CACHE_DOCKER_BUILD ]]; then
-    docker build -t osg_authentication_base osg_authentication/osg_authentication_base 
+    docker build -t osg_authentication_base osg_authentication_base 
 else
-    docker build --no-cache -t osg_authentication_base osg_authentication/osg_authentication_base 
+    docker build --no-cache -t osg_authentication_base osg_authentication_base 
 fi
