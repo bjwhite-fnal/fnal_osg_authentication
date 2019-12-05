aa472559f6155bda17d23d1fd8aa82db89d28bfc
Brandon White <bjwhite@fnal.gov>
2019-12-05 10:16:54 -0600

    Create a flag file to provide a notification to interested parties when done installing the certificates.

--- osg_authentication/docker_entrypoint.sh
+++ osg_authentication/docker_entrypoint.sh
@@ -4,4 +4,7 @@ echo "Starting the Cron daemon..."
 crond
 echo "Ensuring that the osg-ca-certs package is fully installed..."
 yum reinstall -y osg-ca-certs
+echo "Creating flag to inform other containers that the certificates are safe to mount."
+touch /etc/grid-security/certificates/jobs_done
+echo "Created /etc/grid-security/certificates/jobs_done..."
 tail -f /var/log/osg_ca_cron
