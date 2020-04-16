e25b6974ad564bb4bc7a3f1c197ab2ca1607cb0f
Brandon White <bjwhite@fnal.gov>
2020-04-16 14:28:51 -0500

    Force yum update/upgrade to ensure we get the latest CA certificates

--- osg_authentication/docker_entrypoint.sh
+++ osg_authentication/docker_entrypoint.sh
@@ -3,6 +3,7 @@
 echo "Starting the Cron daemon..."
 crond
 echo "Ensuring that the osg-ca-certs package is fully installed..."
+yum -y update && yum -y upgrade
 yum reinstall -y osg-ca-certs
 echo "Creating flag to inform other containers that the certificates are safe to mount."
 touch /etc/grid-security/certificates/jobs_done
